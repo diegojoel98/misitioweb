102a2375463fca6704b4eb0b4dd6acb1b90ec9e1
diegojoel98 <diegojoel98@gmail.com>
2020-03-26 20:20:16 -0600

    Update

--- src/components/contact.form.js
+++ src/components/contact.form.js
@@ -2,14 +2,13 @@ import React from "react";
 
 export default () => (
     <form className="mt-16 text-center" /*action="form.php" method="post"*/>
-        <label htmlFor="contact-content" className="block text-gray-700 font-bold mb-2">¿Que te interesaria realizar?</label>
+        <label htmlFor="contact-content" className="block text-gray-700 font-bold mb-2">¿Qué te interesaria realizar?</label>
         <div className="flex shadow rounded bg-white border p-2">
             <textarea 
                 id="contact-content"
                 name="contact-content"
-                className="flex-1 py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline"></textarea>
-            <button className="btn ml-4">
-            <a href="mailto:diegojoel98@gmail.com">Enviar</a></button>
+                className="flex-1 py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline"></textarea>            
+               <a href="mailto:diegojoel98@gmail.com" className="btn">Enviar</a>    
         </div>
 
     </form>
